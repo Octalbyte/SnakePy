22d45c60087879ea515f90333e053c6663319877
JPSO <66487668+Joao-Pedro-S@users.noreply.github.com>
2020-10-26 11:26:47 -0300

    Create setup.bat
    Setup file for python 2.7 or newer.

---
+++ setup.bat
@@ -0,0 +1,13 @@
+pip install math
+pip install random
+pip install pygame
+pip install tkinter
+pip install notify
+pip install notify_run
+
+pip install Math
+pip install Random
+pip install Pygame
+pip install Tkinter
+pip install Notify
+pip install Notify_run
