a18bf76056cc74905513069ea3ebbe7421525eeb
JPSO <66487668+Joao-Pedro-S@users.noreply.github.com>
2020-10-26 11:43:06 -0300

    Create SETUP.cpp

---
+++ SETUP.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int main()
+{
+  system("
+pip install math
+pip install random
+pip install pygame
+pip install tkinter
+pip install notify
+pip install notify_run
+
+pip install Math
+pip install Random
+pip install Pygame
+pip install Tkinter
+pip install Notify
+pip install Notify_run");
+  return 0;
+}
